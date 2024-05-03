6553c13eaa52a2542a7f14a2831cf0720bcb5842
ParkJinHui <jiinii0104@outlook.com>
2024-05-03 17:58:26 +0900

    Successfully connect the data base. Install the 'koa'

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config({path:'variable.env'});
 const MONGODB_URL = 'mongodb+srv://jiiinhuiii0104:niD4HQXWphfczZOl@cluster0.cnekfui.mongodb.net/'
 
-mongoose.connect(MONGODB_URL, {useNewUrlParser: true}, (err) => {
-    if(err) {
-        console.log('error');
-    } else {
-        console.log('Connected to database successfully');
-    }
-})
+mongoose
+  .connect(MONGODB_URL, { useNewUrlParser: true })
+  .then(() => console.log("Successfully connect."))
+  .catch((e) => console.log(e));
